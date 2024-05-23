e5880063833454c279994ced09d0d8bb7ed2ec73
Kilsunho <sh9567@naver.com>
2024-05-23 10:19:50 +0900

    Fix : ButtenManager 오타수정

--- Assets/Scripts/Manager/ButtenManager.cs
+++ Assets/Scripts/Manager/ButtenManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameController : MonoBehaviour
+public class ButtenManager : MonoBehaviour
 {
    public void EndGame()
     {
