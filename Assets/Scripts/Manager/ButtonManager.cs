af4b90583abfd54845ac97cca4a1e16a8f12a8aa
Kilsunho <sh9567@naver.com>
2024-05-23 10:26:03 +0900

    Fix : ButtonManager
    오타 수정

--- Assets/Scripts/Manager/ButtonManager.cs
+++ Assets/Scripts/Manager/ButtonManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class ButtenManager : MonoBehaviour
+public class ButtonManager : MonoBehaviour
 {
    public void EndGame()
     {
